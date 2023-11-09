5647a5d6ecf5224984c703f7f8fbd969336789d7
HieuTrungCao <21021490@vnu.edu.vn>
2023-11-09 23:18:35 +0700

    Change src, trg lang

--- datasets/my_datasets.py
+++ datasets/my_datasets.py
@@ -1,6 +1,35 @@
-from torchtext.datasets import TranslationDataset
+import os
+import io
 
+from torchtext.datasets import TranslationDataset
+from torchtext.data import Example
 class MyTranslationDataset(TranslationDataset):
-    """
-    code here
-    """
+
+    """Defines a dataset for machine translation."""
+
+    def __init__(self, path, exts, fields, **kwargs):
+        """Create a TranslationDataset given paths and fields.
+
+        Arguments:
+            path: Common prefix of paths to the data files for both languages.
+            exts: A tuple containing the extension to path for each language.
+            fields: A tuple containing the fields that will be used for data
+                in each language.
+            Remaining keyword arguments: Passed to the constructor of
+                data.Dataset.
+        """
+        if not isinstance(fields[0], (tuple, list)):
+            fields = [('src', fields[0]), ('trg', fields[1])]
+
+        src_path, trg_path = tuple(os.path.expanduser(path + x) for x in exts)
+
+        examples = []
+        with io.open(src_path, mode='r', encoding='utf-8') as src_file, \
+                io.open(trg_path, mode='r', encoding='utf-8') as trg_file:
+            for src_line, trg_line in zip(src_file, trg_file):
+                src_line, trg_line = src_line.strip(), trg_line.strip()
+                if src_line != '' and trg_line != '':
+                    examples.append(Example.fromlist(
+                        [src_line, trg_line], fields))
+
+        super(TranslationDataset, self).__init__(examples, fields, **kwargs)
