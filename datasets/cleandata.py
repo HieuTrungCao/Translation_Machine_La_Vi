e1661f7ac5cb36c444f85459d0cb779f96ba34bf
Duong Long <longduongbao29@gmail.com>
2023-11-22 18:38:29 +0700

    clean data #1

--- datasets/cleandata.py
+++ datasets/cleandata.py
@@ -11,24 +11,23 @@ class CleanData:
             self.language_tuple= (data_opt["src_lang"], data_opt["trg_lang"])
         else:
             print("Could load file")
-    @staticmethod
-    def custom_split_tokenize(sentence):
-                """Replace default tokenize
-                """
-                tokens = sentence.strip().split()
-                i=0
-                while i < len(tokens):
-                    # tokens[i] = tokens[i].replace','')
-                    tk= tokens[i]
-                    if re.search(r'[\-\+\*\(\)\{\}\<\>\°\/\\\=\@\#\$\%\^\&\_\[\]\~\`]',tk):
-                        tokens.remove(tk)
-                        i-=1
-                    i+=1
-                return tokens
+    # @staticmethod
+    # def custom_split_tokenize(sentence):
+    #             """Replace default tokenize
+    #             """
+    #             tokens = sentence.strip().split()
+    #             i=0
+    #             while i < len(tokens):
+    #                 tk= tokens[i]
+    #                 if re.search(r'\d|www|http',tk):
+    #                     tokens.remove(tk)
+    #                     i-=1
+    #                 i+=1
+    #             return tokens
     @staticmethod        
     def clean_sentence(sentence):
         special_characters ='"@#$%^*+_\/=<>'
-        end_punct = ',.]>)}?!:'
+        end_punct = ',.]>)}?!:;'
         open_punct = '({[<}'
         for char in special_characters:
             sentence = re.sub(pattern="\\"+char,repl="",string= sentence)
@@ -43,6 +42,7 @@ class CleanData:
         sentence = re.sub(pattern="  "+char,repl=" ",string=sentence)
       
         return sentence
+        
     def clean_file(self, mode):
         data_path=''
         if mode == 'train':
@@ -53,42 +53,51 @@ class CleanData:
 
         clean_src_path, clean_trg_path = tuple(os.path.expanduser(data_path +"_clean"+x) for x in self.language_tuple)
         
-        
-        special_characters ='"@#$%^*+_\/=<>'
-        end_punct = ',.]>)}?!:'
-        open_punct = '({[<}'
-            
+        # special_characters ='"@#$%^*+_\/=<>'
+
+        # end_punct = ',.?!:'
+        # open_punct = '({[<}'
+        #\d+\ *(nm|mm|cm|dm|km|m|kg|h|rmp|Kw|USD|EUR|NA)\b|
+        punct_patt= r'\w+(.|,|:|;|!)'
+        patt = r'[\Ø\″\³\¥\—\°\”\£\★\≤\Φ\Ω\≥\×\/\\\|\“\"\'\{\}\[\]\!\@\#\$\%\^\*\(\)\+\-\=\_\`\~\→\»\【\】\•\™\♦\©\±\⬆️\⬇️\–\<\>\฿\®\€]'
         with io.open(src_path, mode='r', encoding='utf-8') as src_file, \
                 io.open(clean_src_path, mode='w', encoding='utf-8') as clean_src_file:
             content = src_file.read()
-            #Delete special characters
+            #Delete special characters       
+            # for char in special_characters or char in r'[1-9]':
+            #     content = re.sub(pattern="\\"+char,repl="",string= content)
+            # #Add a space infront of puntation characters
+            # for char in end_punct:
+            #     char_pattern = re.escape(char)
+            #     content = re.sub(pattern=char_pattern,repl=" "+char,string=content) 
+            # for char in open_punct:
+            #     char_pattern = re.escape(char)
+            #     content = re.sub(pattern=char_pattern,repl=char+" ",string=content) 
+            #delete duplicate spaces        
+            #delete all nums, special characters, punctations
             
-            for char in special_characters:
-                content = re.sub(pattern="\\"+char,repl="",string= content)
-            #Add a space infront of puntation characters
-            for char in end_punct:
-                char_pattern = re.escape(char)
-                content = re.sub(pattern=char_pattern,repl=" "+char,string=content) 
-            for char in open_punct:
-                char_pattern = re.escape(char)
-                content = re.sub(pattern=char_pattern,repl=char+" ",string=content) 
-            #delete duplicate spaces 
-            content = re.sub(pattern="  "+char,repl=" ",string=content)
+            content = re.sub(r'(?<!\d)(?<!\.{2})([.,:;])(?=\s|$)', r' \1', content)
+          
+            content = re.sub(pattern= patt, repl=" ", string= content)
+            content = re.sub(pattern=r" +",repl=" ",string=content)
             clean_src_file.write(content)
                 
         with io.open(trg_path, mode='r', encoding='utf-8') as trg_file, \
                 io.open(clean_trg_path, mode='w', encoding='utf-8') as clean_trg_file:
             content = trg_file.read()
             #Delete special characters   
-            for char in special_characters:
-                content = re.sub(pattern="\\"+char,repl="",string= content)
-            #Add a space infront of puntation characters
-            for char in end_punct:
-                char_pattern = re.escape(char)
-                content = re.sub(pattern=char_pattern,repl=" "+char,string=content) 
-            for char in open_punct:
-                char_pattern = re.escape(char)
-                content = re.sub(pattern=char_pattern,repl=char+" ",string=content)   
-            content = re.sub(pattern="  "+char,repl=" ",string=content)
+            # for char in special_characters:
+            #     content = re.sub(pattern="\\"+char,repl="",string= content)
+            # #Add a space infront of puntation characters
+            # for char in end_punct:
+            #     char_pattern = re.escape(char)
+            #     content = re.sub(pattern=char_pattern,repl=" "+char,string=content)
+            # for char in open_punct:
+            #     char_pattern = re.escape(char)
+            #     content = re.sub(pattern=char_pattern,repl=char+" ",string=content)
+            content = re.sub(r'(?<!\d)(?<!\.{2})([.,:;])(?=\s|$)', r' \1', content)
+            content = re.sub(pattern= patt, repl=" ", string= content)
+            content = re.sub(pattern=r" +",repl=" ",string=content)
+
             clean_trg_file.write(content)
         print("Files cleaned successfully!")
